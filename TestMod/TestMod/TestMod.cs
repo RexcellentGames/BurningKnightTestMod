5dc4ff8a0fe466d0c9f22a654e23400c770460d9
Egor Dorichev <egordorichev@gmail.com>
2020-02-13 16:04:59 +0300

    First super basic mod

---
+++ TestMod/TestMod/TestMod.cs
@@ -0,0 +1,31 @@
+﻿using BurningKnight.assets;
+using BurningKnight.assets.mod;
+using Lens.graphics;
+using Lens.util;
+using Microsoft.Xna.Framework;
+
+namespace TestMod {
+	public class TestMod : Mod {
+		private float t;
+	
+		public TestMod() {
+			Prefix = "tm";
+		}
+		
+		public override void Init() {
+			Log.Info("YOY");
+		}
+
+		public override void Destroy() {
+			Log.Info("Bye!");
+		}
+
+		public override void Update(float dt) {
+			t += dt;
+		}
+
+		public override void Render() {
+			Graphics.Print($"{t}", Font.Small, new Vector2(4));
+		}
+	}
+}
